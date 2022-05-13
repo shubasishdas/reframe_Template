ac08683f17254322785addae358662af16ae2d3a
bibakroy <bibak68@student.sust.edu>
2022-05-13 20:54:51 +0600

    refactor the theme property and value of wall of love section

--- components/WallOfLove.tsx
+++ components/WallOfLove.tsx
@@ -4,39 +4,38 @@ import Comments from "./Comments";
 
 const WallOfLove = () => {
   return (
-    <>
-      <Container
-        justify="space-between"
-        css={{
-          bg: "#fafafa",
-          mw: "100%",
-          mt: "80px",
-          px: "100px",
-          py: "40px",
-        }}
-      >
-        <Grid.Container css={{ m: "0px auto", p: 0, width: "60vw" }}>
-          <Grid.Container justify="center" direction="column">
-            <Grid>
-              <Text h2 b css={{ ta: "center", mt: "30px" }}>
-                Wall of Love
-              </Text>
-            </Grid>
-            <Grid>
-              <Text size={23} css={{ ta: "center", fontWeight: 300 }}>
-                Lorem ipsum dolor sit amet.
-              </Text>
-            </Grid>
-          </Grid.Container>
-          <Spacer y={2} />
-          <Grid.Container justify="center" gap={4} wrap="wrap">
-            {comments.map((comment, index) => (
-              <Comments key={index} comment={comment} />
-            ))}
-          </Grid.Container>
+    <Container
+      justify="space-between"
+      css={{
+        bg: "$gray50",
+        mw: "$100",
+        mt: "$24",
+        px: "$24",
+      }}
+    >
+      <Grid.Container css={{ w: "$100" }}>
+        <Grid.Container justify="center" direction="column">
+          <Grid>
+            <Text h2 b css={{ ta: "center", mt: "$15" }}>
+              Wall of Love
+            </Text>
+          </Grid>
+          <Grid>
+            <Text size={23} css={{ ta: "center", fontWeight: "$light" }}>
+              Lorem ipsum dolor sit amet.
+            </Text>
+          </Grid>
         </Grid.Container>
-      </Container>
-    </>
+
+        <Spacer y={2} />
+
+        <Grid.Container justify="center" gap={4} wrap="wrap">
+          {comments.map((comment, index) => (
+            <Comments key={index} comment={comment} />
+          ))}
+        </Grid.Container>
+      </Grid.Container>
+    </Container>
   );
 };
 
