fa2dbf8b29248f742a2dca920ee307d2a6b5ab8d
Bibak Roy <89838602+bibakroy@users.noreply.github.com>
2022-05-13 22:24:45 +0600

    refactor the theme property and value of wall of love section (#4)
    * refactor the theme property and value of wall of love section
    
    * removed the responsive props
    
    * changed size property to default

--- components/WallOfLove.tsx
+++ components/WallOfLove.tsx
@@ -1,42 +1,43 @@
-import { Container, Text, Grid, Spacer } from "@nextui-org/react";
+import { Container, Text, Grid, Spacer, Col } from "@nextui-org/react";
 import { comments } from "@utils/comments";
 import Comments from "./Comments";
 
 const WallOfLove = () => {
   return (
-    <>
-      <Container
-        justify="space-between"
-        css={{
-          bg: "#fafafa",
-          mw: "100%",
-          mt: "80px",
-          px: "100px",
-          py: "40px",
-        }}
-      >
-        <Grid.Container css={{ m: "0px auto", p: 0, width: "60vw" }}>
-          <Grid.Container justify="center" direction="column">
-            <Grid>
-              <Text h2 b css={{ ta: "center", mt: "30px" }}>
-                Wall of Love
-              </Text>
-            </Grid>
-            <Grid>
-              <Text size={23} css={{ ta: "center", fontWeight: 300 }}>
-                Lorem ipsum dolor sit amet.
-              </Text>
-            </Grid>
-          </Grid.Container>
-          <Spacer y={2} />
-          <Grid.Container justify="center" gap={4} wrap="wrap">
-            {comments.map((comment, index) => (
-              <Comments key={index} comment={comment} />
-            ))}
-          </Grid.Container>
+    <Container
+      justify="space-between"
+      css={{
+        bg: "$gray50",
+        mw: "$100",
+        mt: "$24",
+        px: "$24",
+      }}
+    >
+      <Grid.Container css={{ w: "$100" }}>
+        <Grid.Container justify="center" direction="column">
+          <Grid>
+            <Text h2 b css={{ ta: "center", mt: "$15" }}>
+              Wall of Love
+            </Text>
+          </Grid>
+          <Grid>
+            <Text size="$sm" css={{ ta: "center", fontWeight: "$light" }}>
+              Lorem ipsum dolor sit amet.
+            </Text>
+          </Grid>
         </Grid.Container>
-      </Container>
-    </>
+
+        <Spacer y={2} />
+
+        <Grid.Container justify="center" gap={4} wrap="wrap">
+          {comments.map((comment, index) => (
+            <Col css={{ w: "$96" }}>
+              <Comments key={index} comment={comment} />
+            </Col>
+          ))}
+        </Grid.Container>
+      </Grid.Container>
+    </Container>
   );
 };
 
